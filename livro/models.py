379e9338a79d75fdec573462cc7b96ffd9e38b69
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-02-27 22:54:33 -0300

    Criando a página de listagem de livros
    Criando a página de listagem de livros. Os livros vão ser listados de acordo com cada usuario que podem ter de 0 ou mais livros

--- livro/models.py
+++ livro/models.py
@@ -1,7 +1,13 @@
 from django.db import models
-
+from usuarios.models import Usuario
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=40)
+    descricao = models.TextField()
 
+    def __str__(self) -> str:
+        return self.nome
+    
 class Livro(models.Model):
     nome = models.CharField(max_length=60)
     autor = models.CharField(max_length=30)
@@ -12,7 +18,8 @@ class Livro(models.Model):
     data_emprestimo = models.DateTimeField(null=True, blank=True)
     data_devolucao = models.DateTimeField(null=True, blank=True)
     tempo_duracao = models.DateField(null=True, blank=True)
-
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
 
     def __str__(self) -> str:
         return self.nome
--- livro/views.py
+++ livro/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from usuarios.models import Usuario
+from .models import Livro
 # Create your views here.
 def home(request):
     user_session = request.session.get('usuario') # Retorna None se essa session não existir
     if user_session:
-        usuario = Usuario.objects.get(id = user_session).username
-        return HttpResponse(f'Olá {usuario}. Abaixo está sua lista de livros.')
+        usuario = Usuario.objects.get(id = user_session)
+        livros = Livro.objects.filter(usuario= user_session)
+        return render(request, 'lista_livros.html', context={'livros': livros, 'usuario':usuario})
     return redirect('/auth/login/?permission=denied')
